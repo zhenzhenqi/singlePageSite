8236c56b867c99384f002e29e9b93f12571028a3
jane363636 <jane363636@gmail.com>
2016-05-09 11:11:19 -0400

    responsive video

--- bgVideo.js
+++ bgVideo.js
@@ -6,11 +6,12 @@ $(document).ready(function () {
 });
 
 function resize_background() {
-    $("#video1").css("min-height", $(window).height() + "px");
+    $("#video1").css("height", $(window).height() + "px");
+    console.log("video_height:"+$(window).height());
     var video_width = parseInt($("#video1").css("width"));
     console.log("video_width:"+video_width);
     var window_width = $(window).width();
-    console.log("window width:"+$(window).width());
+    console.log("window_width:"+$(window).width());
     if (video_width > window_width) {
         var left_pos = ((video_width - window_width) / 2) * (-1);
         $("#video1").css("left", left_pos + "px");
